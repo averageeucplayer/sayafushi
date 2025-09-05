286cfe4b05294a8f8a131913c4e2947191f177b7
Charlatan <legitimatecharlatan@gmail.com>
2025-09-05 21:46:34 +0100

    build.rs

--- app/build.rs
+++ app/build.rs
@@ -1,14 +1,24 @@
 use std::fs;
 
 fn main() {
-    if cfg!(debug_assertions) {
+    #[cfg(debug_assertions)]
+    {
         println!("DEV BUILD");
         tauri_build::build();
-    } else {
+    }
+
+    #[cfg(all(not(debug_assertions), target_os = "windows"))]
+    {
+        use std::fs;
         let mut windows = tauri_build::WindowsAttributes::new();
         windows = windows.app_manifest(fs::read_to_string("windows-app.manifest").unwrap());
+        let attributes = tauri_build::Attributes::new().windows_attributes(windows);
 
-        tauri_build::try_build(tauri_build::Attributes::new().windows_attributes(windows))
-            .expect("failed to run build script");
-    };
+        tauri_build::try_build(attributes).expect("failed to run build script");
+    }
+
+    #[cfg(all(not(debug_assertions), target_os = "linux"))]
+    {
+        tauri_build::build();
+    }
 }
